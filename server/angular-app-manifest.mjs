86d135b06e1e746f581905c0edc58bdccd823414
Mohanad Ghali <mghalix@gmail.com>
2025-09-14 23:22:16 +0300

    latest build

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -4,8 +4,8 @@ export default {
   inlineCriticalCss: true,
   routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 3918, hash: '67fd657854966afee3939ef018db96b0d3a4f8ad02e897f8fb907f884b56e479', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1583, hash: '641f6a0752292b0ceca39c237745e7a52ddb67e4f46b1c4e26c9633a04762d6c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 3918, hash: '16d2a9dc627b55bbdd59485cc989dbd0a58946451ec4c2672ddb8da45ed8c30a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1583, hash: '9132617964a2d99ac4180aac8f7665bd1d17097d7f5d5fba7f720809ae08976a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
 ['styles-K6TNXSD5.css', {size: 402562, hash: 'bdtcHOKMCHc', text: () => import('./assets-chunks/styles-K6TNXSD5_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
