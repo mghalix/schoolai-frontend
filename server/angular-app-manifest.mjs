89e6eff89458da18abc94794e6d345ab3dfe47e3
Mohanad Ghali <mghalix@gmail.com>
2025-09-15 11:22:23 +0300

    latest build

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -4,9 +4,9 @@ export default {
   inlineCriticalCss: true,
   routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 3918, hash: '16d2a9dc627b55bbdd59485cc989dbd0a58946451ec4c2672ddb8da45ed8c30a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1583, hash: '9132617964a2d99ac4180aac8f7665bd1d17097d7f5d5fba7f720809ae08976a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
-['styles-K6TNXSD5.css', {size: 402562, hash: 'bdtcHOKMCHc', text: () => import('./assets-chunks/styles-K6TNXSD5_css.mjs').then(m => m.default)}]
+['index.csr.html', {size: 3918, hash: '9a56e762687027df59e4a2072ae9f860790a5c17592c62dba837b3af82527649', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1583, hash: '037468b62000c60c49865a05786ccc250cd4000ea7fc433566d51048d32b2dbe', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['styles-HS2AK2UU.css', {size: 402371, hash: 'aX5alosG+mw', text: () => import('./assets-chunks/styles-HS2AK2UU_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
 };
